78030a2807abc24e3c7452bf78da44fb7be0a70f
nikenpatel-14 <patelniken3333@gmail.com>
2026-02-02 11:21:26 +0530

    USING AND DISPOSE IMPLEMENTED

---
+++ DAY10/SOLIDandGCAssignment/SOLIDandGCAssignment/UsingDispose.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SOLIDandGCAssignment
+{
+    internal class UsingDispose
+    {
+
+        static void Main(string[] args)
+        {
+            // by implementing using it automatically execute the dispose
+            using(Logger logger = new Logger())
+            {
+                logger.log("file logged");
+
+            }
+        }
+    }
+    class Logger : IDisposable 
+    {
+        public Logger()
+        {
+            Console.WriteLine("log intialized");
+        }
+        public void log(string message) { 
+        
+            Console.WriteLine(message);
+        }
+        //Dispose method for logger 
+        public void Dispose()
+        {
+            Console.WriteLine("dispose called");
+        }
+    }
+
+}
+
