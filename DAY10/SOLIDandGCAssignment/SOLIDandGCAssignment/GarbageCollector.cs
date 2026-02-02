445fcb044b094e2001ea4079a1503d1bd4731ca3
nikenpatel-14 <patelniken3333@gmail.com>
2026-02-02 09:37:33 +0530

    SOLID principle and garbage colletor

---
+++ DAY10/SOLIDandGCAssignment/SOLIDandGCAssignment/GarbageCollector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLIDandGCAssignment
+{
+    class GarbageCollector
+    {
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <3; i++)
+            {
+
+                //on every iteration old object became not reachable
+                FinalizerClass finalizerClass = new FinalizerClass();
+           
+
+            }
+            //USE this for  forcefully garbage colletion   
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            
+        }
+
+
+    }
+    class FinalizerClass
+    {
+        public FinalizerClass()
+        {
+            Console.WriteLine("constructor called");
+        }
+
+        //Declaring finalizer
+        ~FinalizerClass()
+        {
+            Console.WriteLine("finalizer called");
+        }
+    }
+}
---
+++ DAY10/SOLIDandGCAssignment/SOLIDandGCAssignment/SOLIDprinciple.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLIDandGCAssignment
+{
+    internal class SOLIDprinciple
+    {
+       //clinic appointment booking system
+       
+        // Single responsibility principle
+        //appointment service clss just handle bokk appointment
+        class AppointmentService
+        {
+            IAppointmentRepository _repository;
+            INotifier _notifier;
+
+            public AppointmentService(
+                IAppointmentRepository repository,
+                INotifier notifier)
+            {
+                _repository = repository;
+                _notifier = notifier;
+            }
+
+            public void BookAppointment()
+            {
+                _repository.Save();
+                _notifier.Notify();
+            }
+        }
+
+        
+        // Open / closed principle 
+       
+        interface INotifier
+        {
+            void Notify();
+        }
+
+        //can extend inotifier but can not mopify it
+        class EmailNotifier : INotifier
+        {
+            public void Notify()
+            {
+                Console.WriteLine("Email notification sent");
+            }
+        }
+
+        class SmsNotifier : INotifier
+        {
+            public void Notify()
+            {
+                Console.WriteLine("SMS notification sent");
+            }
+        }
+
+        // liskov substitution principle
+      
+        abstract class Doctor
+        {
+            public abstract void Consult();
+        }
+
+
+        //all child class can override parant method
+        class GeneralPhysician : Doctor
+        {
+            public override void Consult()
+            {
+                Console.WriteLine("General consultation");
+            }
+        }
+
+        class Dentist : Doctor
+        {
+            public override void Consult()
+            {
+                Console.WriteLine("Dental consultation");
+            }
+        }
+
+
+        //interface segaregation principle
+
+        //seperate interfacec for consultation and appointment management
+        interface IConsultation
+        {
+            void ConsultPatient();
+        }
+
+        interface IAppointmentManagement
+        {
+            void ManageAppointments();
+        }
+
+        class DoctorStaff : IConsultation
+        {
+            public void ConsultPatient()
+            {
+                Console.WriteLine("Doctor consulting patient");
+            }
+        }
+
+        class Receptionist : IAppointmentManagement
+        {
+            public void ManageAppointments()
+            {
+                Console.WriteLine("Receptionist managing appointments");
+            }
+        }
+
+
+        //dependancy inversion principle
+        interface IAppointmentRepository
+        {
+            void Save();
+        }
+
+        class SqlAppointmentRepository : IAppointmentRepository
+        {
+            public void Save()
+            {
+                Console.WriteLine("Appointment saved to database");
+            }
+        }
+
+        
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                // DIP implememnted
+                IAppointmentRepository repository = new SqlAppointmentRepository();
+                INotifier notifier = new EmailNotifier();
+
+                AppointmentService service =
+                    new AppointmentService(repository, notifier);
+
+                service.BookAppointment();
+
+                // LSP implemented
+                Doctor doctor = new Dentist();
+                doctor.Consult();
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
+
